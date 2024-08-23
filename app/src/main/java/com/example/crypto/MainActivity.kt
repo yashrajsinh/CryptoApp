7d0be1c3002715bee9e753b5b5549d0d98095be9
Yashrajsinh Jadeja <46606790+yashrajsinh@users.noreply.github.com>
2024-08-23 00:13:01 -0400

    Fragment basic done
    Basic Fragment Activity done
    
    - Home
    - Search
    - Profile

--- app/src/main/java/com/example/crypto/MainActivity.kt
+++ app/src/main/java/com/example/crypto/MainActivity.kt
@@ -2,45 +2,42 @@ package com.example.crypto
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.crypto.Fragments.HomeFragment
+import com.example.crypto.Fragments.ProfileFragment
+import com.example.crypto.Fragments.SearchFragment
 import com.example.crypto.ui.theme.CryptoTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CryptoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.ic_home -> makeCurrentFragment(homeFragment)
+                    R.id.ic_search -> makeCurrentFragment(searchFragment)
+                    R.id.ic_profile -> makeCurrentFragment(profileFragment)
                 }
-            }
+            true
         }
-    }
+
+        }
+    //TODO: Method to call Fragment
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoTheme {
-        Greeting("Android")
-    }
-}
+
